10feadfa203e8c8369ca645c88520ea0f8197f21
Khursheed Ali <khursheedali38@gmail.com>
2017-10-13 02:45:20 +0530

    ëxtracting atributes for EDA

--- travel.py
+++ travel.py
@@ -9,18 +9,41 @@ from matplotlib import style
 style.use('ggplot')
 
 #read the data
-path = 'yellow_tripdata_2015-01.csv'
+path = 'random_2015.csv'
 df = pd.read_csv(path)
-df = df.sample(200000)
 df.dropna(inplace=True)
 print(len(df))
 
-
-#writing to file random 200K samples
-df.to_csv('random_2015.csv')
+#Getting attributes for EDA
 df = df[['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'trip_distance']]
+df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+df['pickup_hrs'] = df['tpep_pickup_datetime'].dt.hour
+df['dropoff_hrs'] = df['tpep_dropoff_datetime'].dt.hour
+df['day_week'] = df['tpep_pickup_datetime'].dt.weekday
+df['tpep_pickup_timestamp'] = (df['tpep_pickup_datetime'] - dt.datetime(1970, 1, 1)).dt.total_seconds()
+df['tpep_dropoff_timestamp'] = (df['tpep_dropoff_datetime'] - dt.datetime(1970, 1, 1)).dt.total_seconds() 
+df['duration'] = df['tpep_dropoff_timestamp'] - df['tpep_pickup_timestamp']
+df['speed'] = (df['trip_distance'] * 3600)//df['duration']
 
-print(df.shape)
+#cleaning for EDA
+df = df[ (df['duration'] > 0)]
+df = df[ (df['speed'] > 6.0)]
+df = df[ (df['speed'] < 140.0)]
+
+#write cleaned data to file for Power BI analysis
+df.to_excel('random_2015_cleaned.xlsx')
+
+#EDA
+plt.scatter(df['duration'], df['trip_distance'])
+plt.show()
+
+
+
+
+print(df.dtypes)
+
+
 
 
 
