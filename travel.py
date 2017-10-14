9a36f55dc1e2d0d8543324f17de0d7cd1ededad1
Khursheed Ali <khursheedali38@gmail.com>
2017-10-14 22:36:02 +0530

    clustered pickup and dropoff locations

--- travel.py
+++ travel.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+from sklearn.cluster import KMeans
 from matplotlib import style
 style.use('ggplot')
 
@@ -31,17 +32,32 @@ df = df[ (df['duration'] > 0)]
 df = df[ (df['speed'] > 6.0)]
 df = df[ (df['speed'] < 140.0)]
 
-#write cleaned data to file for Power BI analysis
-df.to_excel('random_2015_cleaned.xlsx')
+###write cleaned data to file for Power BI analysis
+##df.to_excel('random_2015_cleaned.xlsx')
 
-#EDA
-plt.scatter(df['duration'], df['trip_distance'])
+###EDA
+##plt.scatter(df['duration'], df['trip_distance'])
+##plt.show()
+
+#clustering pickup and dropoff locations
+n = len(df)
+kmeans_pickup = KMeans(n_clusters = 15, random_state = 2).fit(df[['pickup_latitude', 'pickup_longitude']])
+df['kmeans_pickup'] = kmeans_pickup.predict(df[['pickup_latitude','pickup_longitude']])
+plt.scatter(df.pickup_longitude[:n], 
+            df.pickup_latitude[:n],
+            cmap = 'viridis',
+            c = df.kmeans_pickup[:n])
+plt.show()
+
+kmeans_dropoff = KMeans(n_clusters = 15, random_state = 2).fit(df[['dropoff_latitude', 'dropoff_longitude']])
+df['kmeans_dropoff'] = kmeans_dropoff.predict(df[['dropoff_latitude','dropoff_longitude']])
+plt.scatter(df.dropoff_longitude[:n], 
+            df.dropoff_latitude[:n],
+            cmap = 'viridis',
+            c = df.kmeans_dropoff[:n])
 plt.show()
 
 
-
-
-print(df.dtypes)
 
 
 
