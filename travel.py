0856ff847199becea14ce4d35cf4ce825074a386
Mohammed Khursheed Ali Khan <khursheedali38@gmail.com>
2017-10-18 01:08:15 +0530

    tuning hyperparameters to increase accuracy - 83%

--- travel.py
+++ travel.py
@@ -88,7 +88,7 @@ X = np.array(df.drop(['duration'], 1))
 y = np.array(df['duration'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
-##clf = RandomForestRegressor(n_jobs = -1)
+##clf = RandomForestRegressor(n_estimators = 50, n_jobs = -1)
 ##clf.fit(X_train, y_train)
 ##
 ####saving the classifier
