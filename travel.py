a1b11dd9ad6a6a150740bd144e074e5a9a10c007
Khursheed Ali <khursheedali38@gmail.com>
2017-10-17 11:51:10 +0530

    pickled up trained model

--- travel.py
+++ travel.py
@@ -2,18 +2,19 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing, cross_validation, svm
-from sklearn.linear_model import LinearRegression
+from sklearn.svm import LinearSVR
+from sklearn.linear_model import LinearRegression, SGDRegressor
 from sklearn.cluster import KMeans
 from matplotlib import style
+import pickle
 style.use('ggplot')
+
 
 #read the data
 path = 'random_2015.csv'
 df = pd.read_csv(path)
 df.dropna(inplace=True)
-print(len(df))
 
 #Getting attributes for EDA
 df = df[['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'trip_distance']]
@@ -31,13 +32,19 @@ df['speed'] = (df['trip_distance'] * 3600)//df['duration']
 df = df[ (df['duration'] > 0)]
 df = df[ (df['speed'] > 6.0)]
 df = df[ (df['speed'] < 140.0)]
+df = df[ (df['pickup_longitude'] != 0)]
+df = df[ (df['dropoff_longitude'] != 0)]
+df = df[ (df['pickup_latitude'] > 38)]
+df = df[ (df['pickup_latitude'] < 45)]
 
-###write cleaned data to file for Power BI analysis
-##df.to_excel('random_2015_cleaned.xlsx')
 
-###EDA
-##plt.scatter(df['duration'], df['trip_distance'])
-##plt.show()
+
+#write cleaned data to file for Power BI analysis
+df.to_excel('random_2015_cleaned.xlsx')
+
+#EDA
+plt.scatter(df['duration'], df['trip_distance'])
+plt.show()
 
 #clustering pickup and dropoff locations
 n = len(df)
@@ -57,40 +64,30 @@ plt.scatter(df.dropoff_longitude[:n],
             c = df.kmeans_dropoff[:n])
 plt.show()
 
-
-###onehot encoding pickup and dropoff clusters
-##enc = preprocessing.OneHotEncoder()
-##enc.fit(df[['kmeans_pickup', 'kmeans_dropoff']])
-##onehotlabels = enc.transform(df[['kmeans_pickup', 'kmeans_dropoff']])
-##print(onehotlabels.shape)
-
 #creating dummy variables/one hot encoding, adding features
-print(df.shape)
 df = pd.concat([df, pd.get_dummies(df['pickup_hrs'], prefix = 'hrs')], axis = 1)
-print(df.shape)
 df = pd.concat([df, pd.get_dummies(df['day_week'], prefix = 'day')], axis = 1)
-print(df.shape)
 df['pickup_dropoff_cluster'] = df['kmeans_pickup'].map(str) + 'to' +  df['kmeans_dropoff'].map(str)
-print(df.shape)
-print(df['pickup_dropoff_cluster'])
 df = pd.concat([df, pd.get_dummies(df['pickup_dropoff_cluster'], prefix = 'route')], axis = 1)
-print(df.shape)
-print(df.head())
 
-print(df.columns)
 ##cleaninig df for training containig only features
 df.drop(df.columns[[0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 14, 15, 47]], axis = 1, inplace = True)
 
-print(df.columns)
-
 #fitting regression algo
 X = np.array(df.drop(['duration'], 1))
-X = preprocessing.scale(X)
 y = np.array(df['duration'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
-clf = LinearRegression()
-clf.fit(X_train, y_train)
+##clf = LinearRegression(n_jobs = -1)
+##clf.fit(X_train, y_train)
+
+##saving the classifier
+##with open('linearregression.pickle', 'wb') as f:
+##    pickle.dump(clf, f)
+
+#load the classifier
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
 accuracy = clf.score(X_test, y_test)
 
 print(accuracy)
